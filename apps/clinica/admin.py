c507541df8c15eac19e104e5071b862149e817c9
leocuco <nelson.cuco@icloud.com>
2024-02-28 21:41:43 +0200

    Model Medico para admin

--- apps/clinica/admin.py
+++ apps/clinica/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import paciente
+from .models import paciente, medico
 
-admin.site.register(paciente)
+admin.site.register(paciente) # Registramos el modelo paciente en el administrador de Django
+admin.site.register(medico) # Registramos el modelo consulta en el administrador de Django
